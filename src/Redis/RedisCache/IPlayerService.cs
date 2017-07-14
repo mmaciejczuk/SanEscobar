735c1b92024a3d8f0fcd89158a42bc1fae2a77d5
Mariusz Maciejczuk <mariusz.maciejczuk@corp.smtsoftware.com>
2017-07-14 11:00:39 +0200

    SEGGUILD-16-Redis-PoC

---
+++ src/Redis/RedisCache/IPlayerService.cs
@@ -0,0 +1,14 @@
+﻿using SanEscobar.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedisCache
+{
+    public interface IPlayerService
+    {
+        string AddPlayer(string gameName, Player player);
+        Player GetPlayer(string playerId);
+        bool DeletePlayer(string playerId);
+    }
+}
---
+++ src/Redis/RedisCache/PlayerService.cs
@@ -0,0 +1,65 @@
+﻿using SanEscobar.Domain.Core;
+using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedisCache
+{
+    public class PlayerService : IPlayerService
+    {
+        public string AddPlayer(string gameId, Player player)
+        {
+            string lastId;
+            try
+            {
+                using (IRedisClient client = new RedisClient())
+                {
+                    var playerClient = client.As<Player>();
+                    var storedPlayer = playerClient.Store(player);
+                    lastId = storedPlayer.Id;
+                    return lastId;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public Player GetPlayer(string playerId)
+        {
+            string lastId;
+            try
+            {
+                using (IRedisClient client = new RedisClient())
+                {
+                    var playerClient = client.As<Player>();
+                    var player = playerClient.GetById(playerId);
+                    return player;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public bool DeletePlayer(string playerId)
+        {
+            try
+            {
+                using (IRedisClient client = new RedisClient())
+                {
+                    var playerClient = client.As<Player>();
+                    playerClient.DeleteById(playerId);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
